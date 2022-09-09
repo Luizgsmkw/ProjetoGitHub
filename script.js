ac959013950658d06eadd3342b4611aad3b594a4
Luizgsmkw <luizgustavokw@gmail.com>
2022-09-09 15:48:55 -0300

    Finalização da pesquisa de repositórios

--- script.js
+++ script.js
@@ -1,77 +1,93 @@
-// const img = document.getElementById("image");
-// const nickName = document.getElementById("nick_name");
-// const nameUser = document.getElementById("name");
-// const dateCreated = document.getElementById("created");
-// const quantidadeRepos = document.getElementById("repos");
-// const followers = document.getElementById("followers");
-// const following = document.getElementById("following");
-const buttonPesquisa = document.getElementById("buttonPesquisa").addEventListener("click", getUsersGit);
-const inputPesquisa = document.getElementById("inp")
-inputPesquisa.addEventListener('keyup', (e) => {
+document.getElementById("buttonPesquisa").addEventListener("click", () => {
+  const userName = document.getElementById("inp").value;
+  getUsersGit(userName);
+});
+
+document.getElementById("inp").addEventListener("keyup", (e) => {
+  const userName = e.target.value;
   const Key = e.which || e.keyCode;
-  const isEnterKeyPressed = Key === 13
-  if(isEnterKeyPressed){
-     getUsersGit()
+  const isEnterKeyPressed = Key === 13;
+  if (isEnterKeyPressed) {
+    getUsersGit(userName);
   }
-})
+});
 
+async function usersGit(userName) {
 
-async function usersGit() {
-  const respostaInput = inputPesquisa.value;
-  const url = `https://api.github.com/users/${respostaInput}`;
+  const url = `https://api.github.com/users/${userName}`;
   const respostaApiGit = await fetch(url);
-  return await respostaApiGit.json()
+  return await respostaApiGit.json();
 }
 
-async function getUsersGit(){
-  usersGit().then((dataUser) => {
+function getUsersGit(userName) {
+  usersGit(userName).then((dataUser) => {
     let infos = `
-    <img src="${dataUser.avatar_url}" alt="Foto perfil" />
-    <div class="container-dados">
+    
             <div class="container-user">
+            <img src="${dataUser.avatar_url}" alt="Foto perfil" class="imgPerfil" />
+    <div class="container-dados">
               <div class="user-name">
                 <p class="nick-name">${dataUser.login}</p>
-                <p class="name">${dataUser.name ?? "Usuário não possui o nome cadastrado🔴 "}</p>
+                <p class="name">${
+                  dataUser.name ?? "Usuário não possui o nome cadastrado🔴 "
+                }</p>
               </div>
-              <p class="date-created">${dataUser.created_at}</p>
             </div>
-            <div class="data">
-              <p class="public_repos">Public_repos<span>${dataUser.public_repos}</span></p>
-              <p class="followers">Followers<span>${dataUser.followers}</span></p>
-              <p class="following">Following<span>${dataUser.following}</span></p>
+            <div class="container-data">
+              <p class="date-created">${dataUser.created_at}</p>
+              <div class="data">
+                <p class="public_repos">Public_repos<span>${
+                  dataUser.public_repos
+                }</span></p>
+                <p class="followers">Followers<span>${
+                  dataUser.followers
+                }</span></p>
+                <p class="following">Following<span>${
+                  dataUser.following
+                }</span></p>
+              </div>
             </div>
           </div>
-    `
-    document.querySelector('.campo-dados').innerHTML = infos
-  })
+    `;
+    document.querySelector(".campo-dados").innerHTML = infos;
+
+    getReposGit(userName);
+  });
 }
 
+async function reposGit(userName) {
+  const respostaApiGit = await fetch(
+    `https://api.github.com/users/${userName}/repos`
+  );
+  return await respostaApiGit.json();
+}
 
-// async function usersGitHub() {
-//   const respostaInput = inputPesquisa.value;
-//   const url = `https://api.github.com/users/${respostaInput}`;
-//   const respostaApiGit = await fetch(url);
-//   const respostaJsonGit = await respostaApiGit.json();
-//   const respostaImg = await respostaJsonGit.avatar_url;
-//   const respostaNickName = await respostaJsonGit.login;
-//   const respostaName = await respostaJsonGit.name;
-//   const respostaDateCreated = await respostaJsonGit.created_at;
-//   const respostaQuantidadeRepos = await respostaJsonGit.public_repos;
-//   const respostaFollowers = await respostaJsonGit.followers;
-//   const respostaFollowing = await respostaJsonGit.following;
-//   img.setAttribute("src", `${respostaImg}`);
-//   nickName.innerText = respostaNickName;
-//   nameUser.innerText = respostaName;
-//   dateCreated.innerText = respostaDateCreated;
-//   quantidadeRepos.innerText = respostaQuantidadeRepos;
-//   followers.innerText = respostaFollowers;
-//   following.innerText = respostaFollowing;
-//   inputPesquisa.value = ''
-// }
+function getReposGit(userName) {
+  reposGit(userName).then((dataRepos) => {
+    let repositories = "";
+    dataRepos.map((repo) => {
+      repositories += `<li class="liResp">
+      <a href="${repo.html_url}" class="linkResp" target="_blank">${repo.name}</a>
+      <span class="spanPrincipal">
+      <span class="span"><img src="img/fork.png" class="imgr"> ${repo.forks}</span>
+      <span class="span"><img src="img/estrela.png" class="imgr"> ${repo.stargazers_count}</span>
+      <span class="span"><img src="img/cuidados-com-os-olhos.png" class="imgr"> ${repo.watchers}</span>
+      <span class="span"><img src="img/pessoa.png" class="imgr"> ${repo.language}</span>
+      </span>
+      </li>`;
+    });
+    document.querySelector(".campo-dados").innerHTML += `
+    <div class="container-repos">
+      <ul class="ulRepos">
+      ${repositories}
+      </ul>
+    </div>`;
+  });
+}
 
-const divRepos = document.getElementById("divRepos");
-const ulRepos = document.getElementById("ulRepos");
-divRepos.appendChild(ulRepos);
+// const divRepos = document.getElementById("divRepos");
+// const ulRepos = document.getElementById("ulRepos");
+// divRepos.appendChild(ulRepos);
 
 // async function reposGitHub() {
 //   let respostaInputrs = inputPesquisa.value;
@@ -152,5 +168,4 @@ divRepos.appendChild(ulRepos);
 //   })
 // }
 // buttonPesquisa.addEventListener("click", reposGitHub);
-//buttonPesquisa.addEventListener("click", getUsersGit);
-
+// //buttonPesquisa.addEventListener("click", getUsersGit);
